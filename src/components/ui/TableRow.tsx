296a76aced83d5508c87b1a21847cf6d039c1f4b
WarinCode <warinsaipanya471@outlook.co.th>
2024-08-30 19:22:31 +0700

    Refactor TableRow component for better readability

--- src/components/ui/TableRow.tsx
+++ src/components/ui/TableRow.tsx
@@ -3,15 +3,37 @@ import { TableRowProps } from "../../types/props";
 
 const TableRow: FC<TableRowProps<HTMLTableRowElement>> = ({
   attributes,
-  statement
+  statement,
 }): ReactElement<HTMLTableRowElement> => {
   return (
     <tr {...attributes} className="text-center border-2 border-black h-12">
-      <td>{statement.infix === null ? <span className="text-pink-600 capitalize">null</span> : statement.infix}</td>
-      <td className="border-x-2 border-black">
-        {statement.operatorStack === null ? <span className="text-pink-600 capitalize">null</span> : statement.operatorStack}
+      <td>
+        {statement.infix === null ? (
+          <span className="text-pink-600 capitalize">null</span>
+        ) : (
+          statement.infix
+        )}
       </td>
-      <td>{"prefix" in statement ? statement.prefix : statement.postfix}</td>
+      <td className="border-x-2 border-black">
+        {statement.operatorStack === null ? (
+          <span className="text-pink-600 capitalize">null</span>
+        ) : (
+          statement.operatorStack
+        )}
+      </td>
+      <td>
+        {"prefix" in statement ? (
+          statement.prefix === null ? (
+            <span className="text-pink-600 capitalize">null</span>
+          ) : (
+            statement.prefix
+          )
+        ) : statement.postfix === null ? (
+          <span className="text-pink-600 capitalize">null</span>
+        ) : (
+          statement.postfix
+        )}
+      </td>
     </tr>
   );
 };
